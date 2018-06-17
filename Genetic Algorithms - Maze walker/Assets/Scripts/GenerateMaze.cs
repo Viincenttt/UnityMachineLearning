4a0aaf1cddd57242eeb4382157777a38b09037cf
Vincent <vincentkok@live.nl>
2018-06-17 17:26:13 +0200

    Added diclaimer that this code was not written by me

--- Genetic Algorithms - Maze walker/Assets/Scripts/GenerateMaze.cs
+++ Genetic Algorithms - Maze walker/Assets/Scripts/GenerateMaze.cs
@@ -1,41 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class GenerateMaze : MonoBehaviour {
+namespace Assets.Scripts {
+    /// <summary>
+    /// Code written by Penny de Byl for the Beginners guide to machine learning with Unity
+    /// </summary>
+    public class GenerateMaze : MonoBehaviour {
 
-	public GameObject blockPrefab;
-	int width = 40;
-	int depth = 40;
+        public GameObject blockPrefab;
+        int width = 40;
+        int depth = 40;
 
-	// Use this for initialization
-	void Awake () {
-		for(int w = 0; w < width; w++)
-		{
-			for(int d = 0; d < depth; d++)
-			{
-				if( w == 0 || d == 0)	//outside walls bottom and left
-				{
-					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y, d + this.transform.position.z), Quaternion.identity);
-				}	
-				else if( w < 3 && d < 3)
-				{
-					continue;
-				}		
-				else if(w == width-1 || d == depth-1) //outside walls top and right
-				{
-					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity);
-				}
-				else if(Random.Range(0,5) < 1)
-				{
-					Instantiate(blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity);
-				}
-			}	
-		}
-	}
+        // Use this for initialization
+        void Awake () {
+            for(int w = 0; w < this.width; w++)
+            {
+                for(int d = 0; d < this.depth; d++)
+                {
+                    if( w == 0 || d == 0)	//outside walls bottom and left
+                    {
+                        Instantiate(this.blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y, d + this.transform.position.z), Quaternion.identity);
+                    }	
+                    else if( w < 3 && d < 3)
+                    {
+                        continue;
+                    }		
+                    else if(w == this.width-1 || d == this.depth-1) //outside walls top and right
+                    {
+                        Instantiate(this.blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity);
+                    }
+                    else if(Random.Range(0,5) < 1)
+                    {
+                        Instantiate(this.blockPrefab, new Vector3(w + this.transform.position.x, this.transform.position.y,d + this.transform.position.z), Quaternion.identity);
+                    }
+                }	
+            }
+        }
 	
-	// Update is called once per frame
-	void Update () {
+        // Update is called once per frame
+        void Update () {
 		
-	}
+        }
+    }
 }
